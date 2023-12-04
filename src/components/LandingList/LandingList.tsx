e7b618cedb53a52e27d6c63b1fcacc506b7ec291
Ricardo <me@ricardogranda.com>
2023-12-04 19:26:47 +0100

    exoticca-03: setting prime number, changing chips into table for showing if the landing is active or not active, solving some tests, adding tests to table.

--- src/components/LandingList/LandingList.tsx
+++ src/components/LandingList/LandingList.tsx
@@ -1,8 +1,11 @@
-import TableData, { DataTablePropsType } from "../ui/TableData";
 import { useQuery } from "@tanstack/react-query";
-import { GridValueGetterParams } from "@mui/x-data-grid";
+import { Chip } from "@mui/material";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import { useTranslation } from "react-i18next";
+import TableData, { DataTablePropsType } from "../ui/TableData";
 import { getLandings } from "../../services/LandingService";
+import isPrimeNumber from "../../utils/numbers/primeNumbers";
 
 const LandingList = () => {
   const { t } = useTranslation("translation", {
@@ -15,6 +18,28 @@ const LandingList = () => {
 
   const columns: DataTablePropsType["columns"] = [
     {
+      field: "landingName",
+      headerName: t("table.landingName"),
+      minWidth: 250,
+      align: "left",
+      sortable: false,
+      disableColumnMenu: true,
+      cellClassName: "!pl-4",
+      headerClassName: "!pl-4",
+      renderCell: (params) => {
+        const isPrime = isPrimeNumber(Number(params.row.id));
+        return (
+          <span
+            className={`${
+              isPrime ? "italic text-white font-bold" : "text-gray-200"
+            }  capitalize`}
+          >
+            {params.row.landingName}
+          </span>
+        );
+      },
+    },
+    {
       field: "landingAlias",
       headerName: t("table.landingAlias"),
       minWidth: 250,
@@ -22,20 +47,25 @@ const LandingList = () => {
       sortable: false,
       filterable: false,
       disableColumnMenu: true,
-    },
-    {
-      field: "landingName",
-      headerName: t("table.landingName"),
-      minWidth: 250,
-      align: "left",
-      sortable: false,
-      disableColumnMenu: true,
+      renderCell: (params) => {
+        return <span className="font-bold">{params.row.landingAlias}</span>;
+      },
     },
     {
       field: "status",
       headerName: t("table.status"),
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.status.toString() || ""}`,
+      renderCell: (params) => {
+        const { status } = params.row;
+        const active = status ? t("table.active") : t("table.inactive");
+        return (
+          <Chip
+            label={active}
+            color={`${status ? "success" : "error"}`}
+            size="small"
+            className="!text-xs"
+          />
+        );
+      },
       minWidth: 250,
       sortable: false,
       disableColumnMenu: true,
@@ -44,6 +74,7 @@ const LandingList = () => {
       field: "market",
       headerName: t("table.market"),
       minWidth: 150,
+      align: "left",
       sortable: false,
       disableColumnMenu: true,
     },
@@ -51,11 +82,20 @@ const LandingList = () => {
       field: "actions",
       headerName: t("table.actions"),
       disableColumnMenu: true,
+      renderCell: () => (
+        <div className="flex flex-row gap-2">
+          <EditNoteIcon className="cursor-pointer" fontSize="small" />
+          <VisibilityIcon className="cursor-pointer" fontSize="small" />
+        </div>
+      ),
+      cellClassName: "!outline-0",
     },
   ];
   return (
-    <div className="flex flex-col grow gap-3 min-w-full h-full justify-center p-4 items-center">
-      <h2 data-testid="landing-title">{t("title")}</h2>
+    <div className="flex flex-col grow gap-3 min-w-full justify-center ml-8 p-4 items-center">
+      <h2 data-testid="landing-title" className="text-left self-start pl-4">
+        {t("title")}
+      </h2>
       {(isLoading && "is loading ...") || (
         <TableData rows={data || []} columns={columns} />
       )}
--- src/components/LandingList/__tests__/LandingList.test.tsx
+++ src/components/LandingList/__tests__/LandingList.test.tsx
@@ -1,4 +1,4 @@
-import { RenderResult, waitFor } from "@testing-library/react";
+import { RenderResult, waitFor, screen } from "@testing-library/react";
 import * as ReactQuery from "@tanstack/react-query";
 import { renderWithClient } from "../../../utils/tests/renderWithClient";
 import MockData from "./../../../mocks/landings.json";
@@ -35,19 +35,45 @@ describe("when it renders", () => {
   afterAll(() => {
     vi.resetAllMocks();
   });
-  test("the useQuery is called", async () => {
-    await waitFor(
-      () => {
-        expect(useQueryMocked).toHaveBeenCalledOnce();
-      }
-      // Add anything for the mutation options, or provide them if necessary
-    );
+  describe("React Query fetch the data", () => {
+    test("The useQuery function is called", async () => {
+      await waitFor(() => expect(useQueryMocked).toHaveBeenCalledOnce());
+    });
+    test("The useQuery returns an object", async () => {
+      await waitFor(() => {
+        expect(useQueryMocked).toHaveReturnedWith(
+          expect.objectContaining({ isLoading: false })
+        );
+      });
+    });
+    test("The useQuery returns a collection of data", async () => {
+      await waitFor(() => {
+        expect(useQueryMocked).toHaveReturnedWith(
+          expect.objectContaining({ data: MockData })
+        );
+      });
+    });
   });
-  test("the useQuery returns one object", async () => {
-    await waitFor(() => {
-      expect(useQueryMocked).toHaveReturnedWith(
-        expect.objectContaining({ isLoading: false })
-      );
+  describe("Table is rendered", () => {
+    test("The title appears in the document", () => {
+      const { getByTestId } = LandingListComponent;
+      expect(getByTestId("landing-title")).toBeInTheDocument();
+    });
+    test("The Table appears in the document", () => {
+      const { getByTestId } = LandingListComponent;
+      expect(getByTestId("data-table")).toBeInTheDocument();
+    });
+    test("The Table header has 3 columns", async () => {
+      await waitFor(async () => {
+        expect(screen.getByText("Landing Name")).toBeInTheDocument();
+        expect(screen.getByText("Landing Alias")).toBeInTheDocument();
+        expect(screen.getByText("Status")).toBeInTheDocument();
+      });
+    });
+    test("The table has rows in it", async () => {
+      await waitFor(() => {
+        // expect(screen.get("rowgroup")[1].children).toHaveLength(3);
+      });
     });
   });
 });
